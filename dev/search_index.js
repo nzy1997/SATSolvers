6ff138aae0e7cfa11772efeaa515a24ab517833c
Documenter.jl <documenter@juliadocs.github.io>
2024-12-10 06:47:39 +0000

    build based on 90a2fa0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SATSolvers","category":"page"},{"location":"#SATSolvers","page":"Home","title":"SATSolvers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SATSolvers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SATSolvers]","category":"page"}]
+}
