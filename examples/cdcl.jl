39fddd87a88840be4efdb6ae5be77d5e6f949e63
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2024-12-17 17:53:04 +0800

    cdcl example

--- examples/cdcl.jl
+++ examples/cdcl.jl
@@ -1,13 +1,12 @@
 using SATSolvers
 # Example from Handbook of Satisfiability Example 4.2.5
-@testset "cdcl examples" begin
-    cl1 = SATClause(8, [8], [3,2])
-    cl2 = SATClause(8, [4], [3])
-    cl3 = SATClause(8, [5], [8,4])
-    cl4 = SATClause(8, [6], [5,1])
-    cl5 = SATClause(8, [7], [5])
-    cl6 = SATClause(8, Int[], [6,7])
 
-    problem = SATProblem([cl1, cl2, cl3, cl4, cl5, cl6])
-    sat,answer = cdcl(problem;viz = true)
-end
+cl1 = SATClause(8, [8], [3,2])
+cl2 = SATClause(8, [4], [3])
+cl3 = SATClause(8, [5], [8,4])
+cl4 = SATClause(8, [6], [5,1])
+cl5 = SATClause(8, [7], [5])
+cl6 = SATClause(8, Int[], [6,7])
+
+problem = SATProblem([cl1, cl2, cl3, cl4, cl5, cl6])
+sat,answer = cdcl(problem;viz = true)
